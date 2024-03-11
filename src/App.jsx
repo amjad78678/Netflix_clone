42b085b6153c7887e5b3ce0f5529352fb45b5645
amjad78678 <81846358+amjad78678@users.noreply.github.com>
2024-03-11 20:59:24 +0530

    after giving redux store

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,28 @@
 import { createContext, useEffect, useState } from 'react'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { RouterProvider, createBrowserRouter, useNavigate } from 'react-router-dom'
 import { onAuthStateChanged } from "firebase/auth";
 import Browse from './components/Browse'
 import Login from './components/Login'
 import Header from './components/Header'
 import Signup from './components/Signup'
 import { auth } from './utils/firebase';
+import { Provider, useDispatch } from 'react-redux';
+import appStore from './store/appStore';
+import { addUser, removeUser } from './store/userSlice';
 
 export const UserContext = createContext(null)
 
 
+
 function App() {
 
+  const dispatch = useDispatch()
 
   const appRouter=createBrowserRouter([
 
     {
       path:'/',
-      element:<Login/>
+      element:<Login/>  
     },
     {
       path:'/signup',
@@ -38,14 +43,19 @@ function App() {
       if (user) {
         // User is signed in, see docs for a list of available properties
         // https://firebase.google.com/docs/reference/js/auth.user
-        const uid = user.uid;
+ 
+        const {uid,email,displayName}=user;
 
-      console.log('iamuserstate',user)
-      setUser(user.displayName)   
+        dispatch(addUser({uid:uid,email:email,displayName:displayName}))
+       console.log('iamuserstate',user)
+       setUser(user.displayName)   
+      
         // ...
       } else {
         // User is signed out
-        // ...
+        
+        dispatch(removeUser())
+       
       }
     });
 
@@ -53,12 +63,13 @@ function App() {
 
   
   return (
-
-   <>
-   <UserContext.Provider value={{user,setUser}} >
-   <RouterProvider router={appRouter}/>
-   </UserContext.Provider>
-   </>
+    <div>
+      <UserContext.Provider value={{ user, setUser }}>
+     
+        <RouterProvider router={appRouter}/>
+   
+      </UserContext.Provider>
+    </div>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,14 @@ import App from './App.jsx'
 import './index.css'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { Provider } from 'react-redux';
+import appStore from './store/appStore.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Provider store={appStore}>
     <App />
     <ToastContainer/>
+    </Provider>
   </React.StrictMode>,
 )
